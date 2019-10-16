955470ce61ff02d62bf15cc3dd9cc2d3678c0ca2
崔丙华 <cuibinghua@joymis.com>
2019-10-16 17:44:38 +0800

    19/10/16

--- python_data_analysis/04-NumPy基础：数组和⽮量计算/4.7-示例-随机漫步.py
+++ python_data_analysis/04-NumPy基础：数组和⽮量计算/4.7-示例-随机漫步.py
@@ -4,3 +4,17 @@
 @file: 4.7-示例-随机漫步.py
 @time: 2019/10/12 17:39
 """
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+
+# 纯Python实现
+position = 0
+walk = [position]
+steps = 1000
+for i in range(steps):
+    step = 1 if random.randint(0, 1) else -1
+    position += step
+    walk.append(position)
+plt.plot(walk[:100])
+plt.show()
