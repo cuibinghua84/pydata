f78fed5761aeb3452853d2129a35f1adb6228da6
崔丙华 <cuibinghua@joymis.com>
2019-10-22 18:24:35 +0800

    2019/10/22

--- python_data_analysis/05-pandas入门/5.3-汇总和计算描述统计.py
+++ python_data_analysis/05-pandas入门/5.3-汇总和计算描述统计.py
@@ -7,9 +7,10 @@
 
 import numpy as np
 import pandas as pd
+from pprint import pprint
 from pandas import Series, DataFrame
+import pandas_datareader.data as web
 
-# 5.3.1 相关性和协方差
 print("")
 df = pd.DataFrame([[1.4, np.nan], [7.1, -4.5], [np.nan, np.nan], [0.75, -1.3]], index=['a', 'b', 'c', 'd'], columns=['one', 'two'])
 print(df)
@@ -63,17 +64,61 @@ diff 			计算第一个算术差值（对时间序列有用）
 pct_change 		计算百分比
 """
 
+# 5.3.1 相关性和协方差
+# print("\n")
+# all_data = {ticker: web.get_data_yahoo(ticker) for ticker in ['AAPL', 'IBM', 'MSFT', 'GooG']}
+# price = pd.DataFrame({ticker: data['Adj Close'] for ticker, data in all_data.items()})
+# volume = pd.DataFrame({ticker: data['Volume'] for ticker, data in all_data.items()})
+# returns = price.pct_change()
+# print(returns.tail())
+
+# print("\n")
+# print(returns['MSFT'].corr(returns['IBM']))
+# print(returns['MSFT'].cov(returns['IBM']))
+# print(returns.MSFT.corr(returns.IBM))
+
+# print("\n")
+# print(returns.corrwith(returns.IBM))
+# print(returns.corrwith(volume))
+
 # 5.3.2 唯一值、计数和成员属性
-# print("\n")
+print("\n")
+obj = pd.Series(['c', 'a', 'd', 'a', 'a', 'b', 'b', 'c', 'c'])
+uniques = obj.unique()
+pprint(uniques)
+pprint(obj.value_counts())
 
-# print("\n")
+print("\n")
+pprint(pd.value_counts(obj.values, sort=False))
 
-# print("\n")
+print("\n")
+pprint(obj)
+mask = obj.isin(['b', 'c'])
+pprint(mask)
+pprint(obj[mask])
 
-# print("\n")
+print("\n")
+to_match = pd.Series(['c', 'a', 'b', 'b', 'c', 'a'])
+unique_vals = pd.Series(['c', 'b', 'a'])
+pprint(pd.Index(unique_vals).get_indexer(to_match))
+"""
+唯一值、计数和集合成员属性方法
+isin：计算表征Series中每个值是否包含于传入序列的布尔值数组
+match：计算数组中每个值的整数索引，形成一个唯一值数组。有助于数据对齐和join类型的操作
+unique：计算Series值中的唯一值数组，按照观察顺序返回
+value_counts：返回一个Series，索引是唯一值序列，值是计数个数，按照个数降序排序
+"""
 
-# print("\n")
+print("\n")
+data = pd.DataFrame({'Qu1': [1, 3, 4, 3, 4], 'Qu2': [2, 3, 1, 2, 3], 'Qu3': [1, 5, 2, 4, 4]})
+print(data)
 
-# print("\n")
+print("\n")
+result = data.apply(pd.value_counts).fillna(0)
+print(result)
 
-# print("\n")
+print("\n")
+
+print("\n")
+
+print("\n")
