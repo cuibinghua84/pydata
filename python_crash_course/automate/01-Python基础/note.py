f4158c87a68172d7c7d890c42df3c4414350cfd7
崔丙华 <cuibinghua@joymis.com>
2019-12-02 17:38:00 +0800

    2019/12/02

--- python_crash_course/automate/01-Python基础/note.py
+++ python_crash_course/automate/01-Python基础/note.py
@@ -4,3 +4,13 @@
 @file: note.py
 @time: 2019/12/2 17:00
 """
+
+print("Hello world!")
+print("What is your name?")
+myName = input()
+print('It is good to meet you, ' + myName)
+print('The length of your name is: ')
+print(len(myName))
+print('What is your age? ')
+myAge = input()
+print('You will be ' + str(int(myAge) + 1) + ' in a year.')
