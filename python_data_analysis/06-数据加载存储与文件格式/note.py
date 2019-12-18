cc67e82ca8c76b7506bb7785556efb31b3273c4c
langqiang <langqiang@joymis.com>
2019-12-19 05:43:16 +0800

    2019/12/19

--- python_data_analysis/06-数据加载存储与文件格式/note.py
+++ python_data_analysis/06-数据加载存储与文件格式/note.py
@@ -9,17 +9,35 @@ from pprint import pprint
 import numpy as np
 import pandas as pd
 from pandas import Series, DataFrame
-import  pandas_datareader.data as web
+import pandas_datareader.data as web
 
 
 def fs():
     print("*" * 50)
 
+
 # 6.1 文本格式数据的读写
+"""
+pandas的解析函数
+pd.read_csv
+pd.read_table
+pd.read_fwf()
+pd.read_clipboard()
+pd.read_excel
+pd.read_hdf()
+pd.read_html()
+pd.read_json()
+pd.read_msgpack()
+pd.read_pickle()
+pd.read_sas()
+pd.read_sql()
+pd.read_stata
+pd.read_feather
+"""
+
 
 # 6.2 二进制格式
 
 # 6.3 与Web API交互
 
 # 6.4 与数据库交互
-
