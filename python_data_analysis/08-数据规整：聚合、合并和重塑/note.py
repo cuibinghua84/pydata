d5cceea4d699ca6775d4214be582877c94d24b6f
langqiang <langqiang@joymis.com>
2019-12-20 05:51:02 +0800

    2019/12/20

--- python_data_analysis/08-数据规整：聚合、合并和重塑/note.py
+++ python_data_analysis/08-数据规整：聚合、合并和重塑/note.py
@@ -15,3 +15,23 @@ from pprint import pprint
 def fs():
     print("*" * 50)
 
+
+# 8.1 分层索引
+data = Series(np.random.randn(9),
+              index=[['a', 'a', 'a', 'b', 'b', 'c', 'c', 'd', 'd'],
+                     [1, 2, 3, 1, 3, 1, 2, 2, 3]])
+print(data)
+
+print(data.index)
+print(data['b'])
+print(data['b': 'c'])
+print(data.loc[['b', 'd']])
+print(data.loc[:, 2])
+
+
+# 8.2 联合与合并数据集
+
+# 8.3 重塑与透视
+
+
+
